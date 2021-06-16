abb2a42c478f87cb38d856f2878e0f79dd14d287
Eion Nelson <ecnelson1@live.com>
2021-06-16 13:19:58 -0700

    added passing endpoint tests for post and get routes for s3 bucket

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,18 +1,23 @@
-const pool = require('../lib/utils/pool');
-const setup = require('../data/setup');
 const request = require('supertest');
-const app = require('../lib/app');
-const AWS = require('aws-sdk-mock');
+const app = require('../lib/app.js');
+// const cors = require('cors');
 
-AWS.mock()
-describe('greeting-art-server routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
+describe( 'Routes for GreetingArt app', () => {
 
-  it('Posts an image to the s3 bucket', async() => {
-    const res = await request(app)
-    .post('/upload')
-    .attach('image', '1b5bb16e-ea99-49e6-b5a4-1b981454abb1.jpg')
+
+  it( 'gets a list of objects from the s3 bucket', async()=> {
+    const response = await request(app)
+    .get('/gallery')
+    expect(response.body).toHaveProperty('Contents')
   })
-});
+
+  it('Allows users to post a file and returns a response from the s3 bucket', async () => {
+    const response = await request(app)
+    .post('/upload')
+    .attach('image','/Users/eionnelson/Alchemy/greeting-art-server/data/assset/1b5bb16e-ea99-49e6-b5a4-1b981454abb1.jpg' )
+    expect(response.body).toHaveProperty('ETag')
+    expect(response.body).toHaveProperty('Bucket', 'greetingart')
+    expect(response.body).toHaveProperty('Location')
+  })
+
+})
