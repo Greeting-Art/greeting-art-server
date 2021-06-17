af63e8b5f382ee3d54a27f082cee4045a9cf93da
Eion Nelson <ecnelson1@live.com>
2021-06-16 17:27:28 -0700

    updated test docs

--- __tests__/sendGrid.test.js
+++ __tests__/sendGrid.test.js
@@ -4,24 +4,41 @@ const app = require('../lib/app');
 
 
 describe("#sendingmailMockedway", () => {
-    it("Should return sent after an email was sent", () => {
+    it("Should return sent after an email was sent", async() => {
+        // const msg = {
+        //     to: `purplebike650@gmail.com`,
+        // from: 'GreetingArt@outlook.com',
+        // subject: `test has send you a Greeting Art`,
+        // text: 'hello there, this is a test' 
+        // }
+        
+        // const response = request(app)
+        // .post('/send')
+        // .send(msg)
+
+        // expect(response._data).toEqual({
+        //     to: 'purplebike650@gmail.com',
+        //     from: 'GreetingArt@outlook.com',
+        //     subject: 'test has send you a Greeting Art',
+        //     text: 'hello there, this is a test'
+        //   })
         const msg = {
-            to: `purplebike650@gmail.com`,
+            to: `test@test.com`,
         from: 'GreetingArt@outlook.com',
         subject: `test has send you a Greeting Art`,
-        text: 'hello there, this is a test' 
+        html: '<h1>hello there</h1>',
+            mail_settings: {
+              sandbox_mode: {
+                enable: true
+              }
+            } 
         }
         
-        const response = request(app)
+        const response = await request(app)
         .post('/send')
         .send(msg)
 
-        expect(response._data).toEqual({
-            to: 'purplebike650@gmail.com',
-            from: 'GreetingArt@outlook.com',
-            subject: 'test has send you a Greeting Art',
-            text: 'hello there, this is a test'
-          })
+        expect(response.status).toEqual(200)
         
         });
     });
